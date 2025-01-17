33c687d66575a80ced516da2bff5573fe8116f2c
Christian Parisca <pariscachristian@gmail.com>
2025-01-16 23:13:26 -0400

    Index principal listo

--- src/app/index-dir.js
+++ src/app/index-dir.js
@@ -1,7 +1,5 @@
 const dirButtons = Array.from(document.querySelectorAll('button'));
 
-
-
 for (const button of dirButtons) {
     button.addEventListener('click', (e) => {
         let dir = e.target.id

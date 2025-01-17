0969cf805de282c759842dd5eda13e8d19b41b29
Christian Parisca <pariscachristian@gmail.com>
2025-01-16 20:10:04 -0400

    commit inicial

---
+++ src/app/index-dir.js
@@ -0,0 +1,10 @@
+const dirButtons = Array.from(document.querySelectorAll('button'));
+
+
+
+for (const button of dirButtons) {
+    button.addEventListener('click', (e) => {
+        let dir = e.target.id
+        window.location.href = `/src/pages/${dir}.html`;
+    })
+}
